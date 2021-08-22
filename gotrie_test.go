4c6775be0056e8e9bfddb30639a1e0f1e86c7b6c
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-08-22 15:23:07 +0700

    test key building

--- gotrie_test.go
+++ gotrie_test.go
@@ -2,6 +2,7 @@ package gotrie
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -11,6 +12,14 @@ var dummies = []struct {
 }{
 	{"name", "john"},
 	{"names", []string{"john", "chena"}},
+}
+
+var keyPathDummies = []struct {
+	Key  string
+	Data interface{}
+}{
+	{"/api/v2/users/:id", "john"},
+	{"/api/v2/users", []string{"john", "chena"}},
 }
 
 func TestNewTrie(t *testing.T) {
@@ -44,6 +53,31 @@ func TestGet(t *testing.T) {
 		value := trie.Get(dummy.Key)
 		if !reflect.DeepEqual(value, dummy.Data) {
 			t.Errorf("trie should contains %s, got %s", dummy.Data, value)
+		}
+	}
+}
+
+func TestBuildKey(t *testing.T) {
+	trie := NewTrie(Config{})
+
+	for _, dummy := range dummies {
+		keys := trie.buildKey(dummy.Key)
+
+		if len(keys) != len(dummy.Key) {
+			t.Errorf("trie key length should %d, got %d", len(keys), len(dummy.Key))
+		}
+	}
+
+	trie = NewTrie(Config{
+		Separator: "/",
+	})
+
+	for _, dummy := range keyPathDummies {
+		keys := trie.buildKey(dummy.Key)
+		t.Log(keys)
+
+		if len(keys) != len(strings.Split(dummy.Key, "/")) {
+			t.Errorf("trie key length should %d, got %d", len(keys), len(dummy.Key))
 		}
 	}
 }
